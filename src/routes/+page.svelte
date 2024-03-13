90d6d64294a88ea45fcb2b8c299bae51ee879516
gui18br <guilhermenovaislimaa@hotmail.com>
2024-03-12 21:13:10 -0300

    feat: add lib components.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
-    import Button from "./Button.svelte";
+    import Teste from "$lib/Teste.svelte";
 </script>
-<h1>Meu site</h1>
 
-<Button label="botao 1"/>
-<Button label="botao 2"/>
-<Button label="botao 3"/>
-<Button label="botao 4"/>
+<h1>Meu site</h1>
+<Teste />
+<Teste />
+<Teste />
+<Teste />
+
 
--- src/routes/Button.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    export let label = "CLICK";
-</script>
-
-<button class="bg-red-500 text-white rounded-lg p-2">
-   {label}
-</button>
-
-<style></style>
