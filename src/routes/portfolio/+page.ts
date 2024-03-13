765abe6f2936004d75bf61f38074a520bec9bfca
gui18br <guilhermenovaislimaa@hotmail.com>
2024-03-13 17:54:57 -0300

    feat: add load function.

---
+++ src/routes/portfolio/+page.ts
@@ -0,0 +1,12 @@
+export function load() {
+  return {
+    items: [
+      { title: "Item 1", category: "Componentes", image: "" },
+      { title: "Item 1", category: "Componentes", image: "" },
+      { title: "Item 1", category: "Componentes", image: "" },
+      { title: "Item 1", category: "Componentes", image: "" },
+      { title: "Item 1", category: "Componentes", image: "" },
+      { title: "Item 1", category: "Componentes", image: "" },
+    ],
+  };
+}
